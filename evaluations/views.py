347395712c665d4c1ce809f47b905adf0c446c86
gyuhwanhwang <rbghks0327@gmail.com>
2021-11-23 04:06:59 +0900

    #3.1 virtual env re-setting

--- evaluations/views.py
+++ evaluations/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("eval main")
+
+
+def eval_home(requset):
+    return render(request, 'eval_home.html')
